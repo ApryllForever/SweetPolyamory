a6d33eaa6e98cb46421b3e54fc905ca47653aa35
Apryll Callahan <ApryllForever@yahoo.com>
2024-02-13 22:38:06 -0800

    Add project files.

---
+++ ModEntry.cs
@@ -0,0 +1,40 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace SweetPolyamory
+{
+    public class Mod : StardewModdingAPI.Mod
+    {
+        public static Mod instance;
+
+        internal static IMonitor ModMonitor { get; set; }
+
+        internal static IModHelper ModHelper { get; set; }
+
+        public override void Entry(IModHelper helper)
+        {
+
+            instance = this;
+
+
+
+
+
+
+            var harmony = new Harmony(ModManifest.UniqueID);
+
+            harmony.PatchAll();
+
+
+        }
+
+
+
+
+        }
+    }
